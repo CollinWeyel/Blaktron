4e3d8232343b352efa06f76c686d7a251e826a65
theDiamondDragon <18754933+DiamandDragon@users.noreply.github.com>
2019-05-16 11:56:22 +0200

    Create folders if they don't exist

--- serial_output.py
+++ serial_output.py
@@ -1,6 +1,9 @@
 #A serial Output for the mido library
 
 from mido.ports import BaseOutput
+import serial
+import time
+import array as arr
 
 def get_devices():
     return [{'name': 'Serial Port', 'isInput': False, 'isOutput': True}]
@@ -9,10 +12,28 @@ class Output(BaseOutput):
     def _open(self, **kwargs):
         device = get_devices()[0]
 
+        self.ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, rtscts=False, dsrdtr=False)
+        print(self.ser.name)
+
         if self.name is None:
             self.name = device['name']
         elif self.name != device['name']:
             raise ValueError('unknown port {!r}'.format(self.name))
 
+        time.sleep(15)
+
     def _send(self, message):
-        print(message.hex())
+        print(message)
+        print(message.bytes())
+        if (len(message.bytes()) == 3):
+            m = bytes([message.bytes()[0]])
+            self.ser.write(m)
+            print(m)
+
+            m = bytes([message.bytes()[1]])
+            self.ser.write(m)
+            print(m)
+
+            m = bytes([message.bytes()[2]])
+            self.ser.write(m)
+            print(m)
